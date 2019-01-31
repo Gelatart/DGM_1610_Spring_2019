c0b37aedfd77f2217bf5be2f860f8de04ae26322
Daniel Wood <daniel.f.wood@gmail.com>
2019-01-30 17:43:39 -0700

    Unity Update for 1/30/2019
    Added a simple (unfinished) animation, basic physics, a particle effect, and UI and event, all on campus

---
+++ DGM 1610 First Game/Assets/PangolinEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PangolinEvent : MonoBehaviour
+{
+
+	public UnityEvent Event;
+
+	// Use this for initialization
+	void OnCollisionEnter2D ()
+	{
+		Event.Invoke();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
