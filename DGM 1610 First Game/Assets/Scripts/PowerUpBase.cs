8a1d8c110d4fa0e9c1372b38de2ea1f7726bacd6
Daniel Wood <daniel.f.wood@gmail.com>
2019-02-20 23:53:34 -0700

    Started implementing power ups
    At home, 2/20/2019

---
+++ DGM 1610 First Game/Assets/Scripts/PowerUpBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PowerUpBase : ScriptableObject
+{
+    
+    public float Value = 10.5f;
+    
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        Value = 20;
+    }
+}
