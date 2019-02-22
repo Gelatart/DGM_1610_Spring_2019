d9d0ee33fe3d59571e5583ee85462ffe4830401b
Daniel Wood <daniel.f.wood@gmail.com>
2019-02-21 23:07:56 -0700

    Added classes that inherit from PowerUpBase
    At home, 2/21/2019

---
+++ DGM 1610 First Game/Assets/Scripts/Health.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Health : PowerUpBase
+{
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        Value = 25;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DGM 1610 First Game/Assets/Scripts/Shield.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : PowerUpBase
+{
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        Value = 15;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
