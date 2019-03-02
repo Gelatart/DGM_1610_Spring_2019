a411000c1aba2e89d1d3df786577ae1856f1e124
Daniel Wood <daniel.f.wood@gmail.com>
2019-03-01 21:57:55 -0700

    Started implementing characterdata and door
    At friend's home, 3/1/2019

---
+++ DGM 1610 First Game/Assets/Scripts/CharacterData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CharacterData : ScriptableObject
+{
+    //public character Value;?
+    //Need to fill in what it actually needs
+}
---
+++ DGM 1610 First Game/Assets/Scripts/Door.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.events;
+
+public class Door : MonoBehaviour
+{
+    public bool IsOpen;
+    public bool WillOpen;
+    //make it so if it is open and activate, it closes and vice versa, using if statements
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
