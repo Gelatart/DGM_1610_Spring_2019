7fa59fe164f44ea8b47400f98c2e6d13a482819d
Daniel Wood <daniel.f.wood@gmail.com>
2019-03-02 23:56:46 -0700

    Begun start of implementing LightSwitch class
    At home, 3/2/2019

---
+++ DGM 1610 First Game/Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LightSwitch : MonoBehaviour
+{
+    //Figure out way to make bools or whatever that will cause light switch event to activate
+    public bool LightActivated;
+    public bool IsActivated = false;
+    public UnityEvent ActivateLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsActivated = false && LightActivated = true)
+        {
+            ActivateLight.Invoke();
+        }   
+    }
+}
