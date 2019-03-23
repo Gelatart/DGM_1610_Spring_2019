ac2368a916715f615b017362d03eb8a87a686d24
Daniel Wood <daniel.f.wood@gmail.com>
2019-03-22 23:57:29 -0600

    Added some more to catch up to where in class (FloatData)
    At home, 3/22/2019

--- DGM 1610 First Game/Assets/Scripts/FloatData.cs
+++ DGM 1610 First Game/Assets/Scripts/FloatData.cs
@@ -5,5 +5,19 @@ using UnityEngine;
 [CreateAssetMenu]
 public class FloatData : ScriptableObject
 {
-    public float Value;
+    [SerializeField] private float value; // visible in editor?
+    public float Value { //visible in events system
+    	get { return value; }
+    	set { this.value += value; }
+    }
+    
+    public void UpdateValue(float floatInput) {
+	    Value += floatInput;	
+    }
+
+    public void UpdateValue(FloatData dataObj)
+    {
+	    Value += dataObj.Value;
+    }
+
 }
