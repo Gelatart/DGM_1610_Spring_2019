471411f4b1f0ee8a5e4b02ef28a610415c3ae99f
Daniel Wood <daniel.f.wood@gmail.com>
2019-03-25 12:09:17 -0600

    Implemented what did in class 3/25
    On campus, 3/25

---
+++ DGM 1610 First Game/Assets/Scripts/FunctionWithParams.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunctionWithParams : MonoBehaviour
+{
+
+    void Start()
+    {
+        AddData(12.5f, 8.5f);
+    }
+
+    private void AddData(float a, float b)
+    {
+        print(a + b);
+    }
+
+    private void OnTriggerEnter(Collider obj)
+    {
+        obj.gameObject.SetActive(false);
+    }
+    
+ }
