66c62e7589541085f774aecf68739bca4b3b0672
Daniel Wood <daniel.f.wood@gmail.com>
2019-04-06 23:58:06 -0600

    Tried to troubleshoot some bug fixing
    At home, 4/6/2019

--- DGM 1610 First Game/Assets/Scripts/Collection.cs
+++ DGM 1610 First Game/Assets/Scripts/Collection.cs
@@ -28,7 +28,8 @@ public class Collection : ScriptableObject
            if(obj.name == "Ammo")
            {
                Debug.Log("We have ammo");
-               Debug.Log("We have " + obj.Value + " ammo.");
+               //Debug.Log("We have " + obj.Value + " ammo.");
+               Debug.Log("We have " + obj.GetType().GetProperty("Count").GetValue(obj) + " ammo."); //Testing? Make sure to list Ammo as "Count" in the object
                //AFTER TESTING: Change "We have ammo" to "We Have " + obj.Value + " ammo."
            }
        }
