ba0068d534a8e1cd87404ddf643aa2b8f49622d7
Daniel Wood <daniel.f.wood@gmail.com>
2019-04-11 22:55:40 -0600

    Tried to build out some classes
    At home, 4/11/2019

--- DGM 1610 First Game/Assets/Scripts/CharacterData.cs
+++ DGM 1610 First Game/Assets/Scripts/CharacterData.cs
@@ -8,4 +8,7 @@ public class CharacterData : ScriptableObject
     //public character Value;?
     //Need to fill in what it actually needs
     public Animal PlayerCharacter;
+    public FloatData Health;
+    public FloatData Ammo;
+    public FloatData Energy;
 }
--- DGM 1610 First Game/Assets/Scripts/UpdateBar.cs
+++ DGM 1610 First Game/Assets/Scripts/UpdateBar.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class UpdateBar : MonoBehaviour
 {
     private Image BarImage;
     public FloatData FillNumber;
+    public UnityEvent IncrementEvent, DecrementEvent;
+    public bool IsAffected;
+    public bool IsPositive;
     
     // Start is called before the first frame update
     void Start()
@@ -18,6 +22,24 @@ public class UpdateBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //BarImage.fillAmount; //Not complete?
+        //BarImage.fillAmount; //
+        if (IsAffected)
+        {
+            EffectDeterminant();
+            IsAffected = false;
+        }
     }
+
+    public void EffectDeterminant()
+    {
+        if (IsPositive)
+        {
+            IncrementEvent.Invoke();
+        }
+        else
+        {
+            DecrementEvent.Invoke();
+        }
+    }
+    //Touch up this code that determines whether to increment or decrement a bar value? Move it somewhere else?
 }
