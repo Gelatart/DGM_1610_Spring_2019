c9a9be50e557b29a341e1eed3443e0bf36b1a032
Daniel Wood <daniel.f.wood@gmail.com>
2019-04-17 11:52:01 -0600

    Added RunAnims function like in class
    On campus, 4/17/2019

---
+++ DGM 1610 First Game/Assets/Scripts/RunAnims.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class RunAnims : MonoBehaviour
+{
+    private Animator anims;
+    public float Speed;
+
+    void Start()
+    {
+        anims = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        anims.SetFloat("Speed," Speed);
+    }
+}
